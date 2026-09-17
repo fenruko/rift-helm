f1134d70db44a0bd65aa7b00c6e1aa5991f46778
Ali <alimzakaria61@gmail.com>
2026-09-17 21:09:50 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function Protected({ children }) {
 export default function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/staff">
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/appeal/:token" element={<AppealThreadPage />} />
